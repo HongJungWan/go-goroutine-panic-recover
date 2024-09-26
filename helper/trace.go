645c3c295cb8cdd5dfbe2f111ea28eb1fa6be8da
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-09-26 17:01:30 +0900

    feat: helper 함수 구현

---
+++ helper/trace.go
@@ -0,0 +1,14 @@
+package helper
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func GetGoroutineID() int {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	var id int
+	fmt.Sscanf(string(buf[:n]), "goroutine %d ", &id)
+	return id
+}
